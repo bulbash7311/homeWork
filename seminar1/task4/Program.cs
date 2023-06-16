3b3205fd09c25e33249b957c90c9a1b574fbe4c5
bulbash <mdv73rus@yandex.ru>
2023-06-16 10:15:03 +0400

    added seminar 1 homework

---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int first = (numbers / 100);
+int second = (numbers / 10) % 10;
+int third = numbers % 10;
+
+// Console.WriteLine (first + " " + second + " " + third);
+
+if (first > second && first > third)
+{
+    Console.WriteLine ("max = " + first);
+} else 
+
+if (second > first && second > third)
+{
+    Console.WriteLine ("max = " + second);
+} else 
+
+if (third > first && third > second)
+{
+    Console.WriteLine ("max = " + third);
+} else
+
+if( first == second)
+{
+    Console.WriteLine ("Числа равны");
+}
---
+++ seminar1/task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.Write(" Yes ");
+}
+else Console.Write(" No ");
