0db866d39ccb8b1934a9cc13147c6a526ff5aee9
bulbash <bulbash@bulbBook.local>
2023-06-23 17:03:00 +0400

    fix homeWork3

--- seminar3/task21/Program.cs
+++ seminar3/task21/Program.cs
@@ -10,13 +10,13 @@ aCoordinate[1] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Z первой точки: ");
 aCoordinate[2] = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите X первой точки: ");
+Console.WriteLine("Введите X второй точки: ");
 bCoordinate[0] = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите Y первой точки: ");
+Console.WriteLine("Введите Y второй точки: ");
 bCoordinate[1] = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите Z первой точки: ");
+Console.WriteLine("Введите Z второй точки: ");
 bCoordinate[2] = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((bCoordinate[0] - aCoordinate[0]), 2) + Math.Pow((bCoordinate[1] - aCoordinate[1]), 2) + Math.Pow((bCoordinate[2] - aCoordinate[2]), 2)),2));
