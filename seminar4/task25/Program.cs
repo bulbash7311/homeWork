9dd0e1c7c2fc2da5eccafc94ee132ac5e3ab0576
bulbash <bulbash@bulbBook.local>
2023-06-24 16:12:00 +0400

    added seminar4 homework

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Math.Pow(a, b));
---
+++ seminar4/task27/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = Prompt("Введите число:");
+
+int Prompt(string message)
+{
+    System.Console.Write("Введите число");
+    int result = Convert.ToInt32(Console.Readline());
+    return result;
+}
+
+ int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+
+    return result;
+}
+
+Console.WriteLine($"Сумма всех чисел {number} = {SumAllDigit(number)}");
