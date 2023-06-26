1204af059d1e12c089e311a907983b304ea1a449
bulbash <mdv73rus@yandex.ru>
2023-06-26 19:48:25 +0400

    added task29

--- seminar4/task29/Program.cs
+++ seminar4/task29/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+
+    return result;
+}
+
+int[] ArrayGenerator(int length, int min, int max)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void printArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+        System.Console.Write($"{array[array.Length - 1]}");
+        System.Console.Write("]");
+}
+int num = ReadInt("Введите размер массива: ");
+int numMin = ReadInt("Введите начальное значение массива: ");
+int numMax = ReadInt("Введите конечное значение массива: ");
+
+int[] array = ArrayGenerator(num, numMin, numMax);
+
+printArray(array);
