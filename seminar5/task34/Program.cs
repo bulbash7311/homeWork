d2c1651f10acedf3ce600d2be9d3ca7f422bc9b9
bulbash <mdv73rus@yandex.ru>
2023-07-01 10:51:19 +0400

    added seminar5 homework

---
+++ seminar5/task34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. 
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите количество элементов массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+void arr(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = new Random().Next(99, 999);
+        Console.Write(array[i] + " ");
+    }
+}
+
+int num(int[] array)
+{
+    int num = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            num = num + 1;
+    }
+    return num;
+}
+
+arr(a);
+Console.Write($"\nКоличество чётных чисел в массиве: {num(array)}");
---
+++ seminar5/task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. 
+// [3, 7, 23, 12] -> 19 [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите количество элементов массива:");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+void arr(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+int num(int[] randomArray)
+{
+    int sum = 0;
+    int i = 0;
+    while (i < randomArray.Length)
+    {
+        sum = sum + randomArray[i];
+        i = i + 2;
+    }
+    return sum;
+}
+
+arr(a);
+Console.Write($"\nCумма элементов, стоящих на нечётных позициях: {num(array)}");
