d3ad479fa20be0202d5057d19b332e61ffa3f5d8
bulbash <mdv73rus@yandex.ru>
2023-07-02 15:13:02 +0400

    added new task52

--- seminar7/task52/Program.cs
+++ seminar7/task52/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(rows, cols, 0, 10);
+PrintMatrix(matrix);
+
+double[] sum = new double[cols];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum[j] = sum[j] + matrix[i, j];
+    }
+}
+
+System.Console.WriteLine("");
+
+for (int i = 0; i < sum.Length; i++)
+{
+    System.Console.Write(sum[i] / 2 + "\t");
+}
