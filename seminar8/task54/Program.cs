0704e19182d913e20a11f0eebb1c14a73b5b495e
bulbash <mdv73rus@yandex.ru>
2023-07-08 22:08:28 +0400

    added task54

--- seminar8/task54/Program.cs
+++ seminar8/task54/Program.cs
@@ -1,1 +1,69 @@
 ﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int rows, int cols, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[rows, cols];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(rows, cols, 0, 10);
+int[] array = new int[cols];
+int count = 0;
+PrintMatrix(matrix);
+
+System.Console.WriteLine();
+System.Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        count = 0;
+
+        while (count < cols - 1)
+        {
+            int num = matrix[j, count];
+
+            if (num < matrix[j, count + 1])
+            {
+                matrix[j, count] = matrix[j, count + 1];
+                matrix[j, count + 1] = num;
+            }
+
+            count++;
+        }
+    }
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine();
+PrintMatrix(matrix);
