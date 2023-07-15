2a1c77e1271a15da06e56bca6988bc1c0d7d1147
bulbash <mdv73rus@yandex.ru>
2023-07-15 15:46:13 +0400

    add task 66

--- seminar9/task66/Program.cs
+++ seminar9/task66/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+int num = 0;
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Numbers(int M, int N)
+{
+    num = num + N;
+
+    if (N == M)
+    {
+        System.Console.Write(num);
+        return;
+    }
+
+    Numbers(M, N - 1);
+}
+
+
+Numbers(M,N);
